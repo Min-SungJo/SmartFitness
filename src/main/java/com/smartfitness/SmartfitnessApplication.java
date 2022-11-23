03aa0294d9f660ee8fa0ac8dd18fee8882ef425c
Min-SungJo <123or@naver.com>
2022-11-23 18:41:55 +0900

    first commit

---
+++ src/main/java/com/smartfitness/SmartfitnessApplication.java
@@ -0,0 +1,13 @@
+package com.smartfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartfitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartfitness/SmartfitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
