2fd45203f0e9a330d9b6436b4b1389d04aa2936f
Min-SungJo <123or@naver.com>
2022-11-24 17:57:08 +0900

    Cors 세팅

---
+++ src/main/java/com/smartfitness/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.smartfitness.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("home")
+@RestController
+public class MainController {
+    @GetMapping("/main")
+    public String main(){
+        return "main";
+    }
+}
