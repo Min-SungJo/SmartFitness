2b9216c3da469249de5d4a1083b5e6ac10f112b4
Min-SungJo <123or@naver.com>
2022-12-01 19:58:18 +0900

    회원 수정, 탈퇴 기능

--- src/main/java/com/smartfitness/mapper/MemberMapper.java
+++ src/main/java/com/smartfitness/mapper/MemberMapper.java
@@ -1,5 +1,7 @@
 package com.smartfitness.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.smartfitness.model.Member;
@@ -9,5 +11,14 @@ public interface MemberMapper {
 
     /**회원가입 */
     void join(Member member);
+
+    /**로그인 */
+    Map<String,Object> login(Map<String, String> param);
+
+    /**회원정보 수정 */
+    void update(Map<String, String> param);
+
+    /**회원 탈퇴 */
+    void delete(Map<String, String> param);
     
 }
--- src/main/java/com/smartfitness/service/MemberService.java
+++ src/main/java/com/smartfitness/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.smartfitness.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,24 @@ public class MemberService {
     @Autowired
     MemberMapper memberMapper;
 
+    /**회원가입 */
     public void join(Member member) {
         memberMapper.join(member);
+    }
+
+    /**로그인 */
+    public Map<String,Object> login(Map<String, String> param) {
+        return memberMapper.login(param); 
+    }
+
+    /**회원정보 수정 */
+    public void update(Map<String, String> param) {
+        memberMapper.update(param);
+    }
+
+    /**회원탈퇴 */
+    public void delete(Map<String, String> param) {
+        memberMapper.delete(param);
     }
     
 }
