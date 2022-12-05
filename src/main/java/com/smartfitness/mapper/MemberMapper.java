1374b5fac75b3032831728e27019dec8a965c6a3
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2022-12-05 18:00:44 +0900

    Revert "Back member"

--- src/main/java/com/smartfitness/mapper/MemberMapper.java
+++ src/main/java/com/smartfitness/mapper/MemberMapper.java
@@ -1,24 +1,8 @@
 package com.smartfitness.mapper;
 
-import java.util.Map;
-
 import org.apache.ibatis.annotations.Mapper;
-
-import com.smartfitness.model.Member;
 
 @Mapper
 public interface MemberMapper {
-
-    /**회원가입 */
-    void join(Member member);
-
-    /**로그인 */
-    Map<String,Object> login(Map<String, String> param);
-
-    /**회원정보 수정 */
-    void update(Map<String, String> param);
-
-    /**회원 탈퇴 */
-    void delete(Map<String, String> param);
     
 }
